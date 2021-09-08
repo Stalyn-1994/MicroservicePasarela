28b27a6c3fddf8b250f317ce5af33ef22798f28f
Stalyn-1994 <stalyn.vaz@gmail.com>
2021-09-08 18:13:20 -0500

    ok

--- src/main/java/com/Trainee/rest/GetTransaccion.java
+++ src/main/java/com/Trainee/rest/GetTransaccion.java
@@ -1,16 +1,22 @@
 package com.Trainee.rest;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.Trainee.model.transaccionfinal;
 import com.Trainee.services.ServicioTransaction;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 @RequestMapping(path="/rest/dataTrainee")
 public class GetTransaccion {
 	
@@ -19,20 +25,23 @@ public class GetTransaccion {
 
 	
 	@GetMapping
-	private ResponseEntity<List<transaccionfinal>> listarTodasLasPersona (){
-		return ResponseEntity.ok(servicio.getAllPersonas());
+	private ResponseEntity<List<transaccionfinal>> listarTodasLasPersona (){	
+			return ResponseEntity.ok(servicio.getAllPersonas());			
 	}
 
 
 	@PostMapping
-	private String guardar (@RequestBody transaccionfinal persona){
+	private String guardar (@RequestBody transaccionfinal persona){		
 		String temporal = servicio.create(persona);
-		
 		try {
-			return temporal;
+			
+				
+				return temporal;				
 			
 		}catch (Exception e) {
 			return null;
 		}
 	}
+	
+	
 }
