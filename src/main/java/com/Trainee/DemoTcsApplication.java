805ceccca8930b84d4eb6727a1472b6ea0b81dcb
Nicolás Arias <josue.arias@epn.edu.ec>
2021-09-09 11:28:47 -0500

    Uncomment eureka config

--- src/main/java/com/Trainee/DemoTcsApplication.java
+++ src/main/java/com/Trainee/DemoTcsApplication.java
@@ -3,10 +3,10 @@ package com.Trainee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
-//@EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class DemoTcsApplication {
 
